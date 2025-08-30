d735f9b0640251231cb49c786248f3702b4997a2
DESKTOP-74GR2ON\user <kinemura@naver.com>
2025-08-31 08:43:58 +0900

    add 01-study

---
+++ 01-study/01-02-python-base.py
@@ -0,0 +1,27 @@
+a = 1
+b = 2
+print(a + b)
+
+a = 3
+# if문
+if a > 1:
+    print("a는 1보다 큽니다.")
+
+# for문
+for a in [1, 2, 3]:
+    print(a)
+
+# while문
+i = 0
+while i < 6:
+    i = i + 1
+    print(i)
+
+def add(a, b):
+    return a + b
+
+print(add(3, 4))
+
+"""
+안녕하세요, 주석입니다.
+"""
---
+++ 01-study/01-04-python-string.py
@@ -0,0 +1,8 @@
+# 정상 출력
+food = "Python's favorite food is perl"
+print(food)
+
+
+# 비정상 출력
+# food = 'Python's favorite food is perl'
+# print(food)
