00a5ada31f42f345a361bbe21ab6e891d4aca91b
DESKTOP-74GR2ON\user <kinemura@naver.com>
2025-09-09 21:07:48 +0900

    add 6일차 공부

---
+++ 06-study/06-01-module-study/index.py
@@ -0,0 +1,11 @@
+# import module1
+#
+# print(module1.add(3,4))
+# print(module1.sub(3,4))
+
+# from module1 import add
+# print(add(5,5))
+# print(sub(3,5)) # 에러
+
+from module1 import *
+# print (sub(4,4))
---
+++ 06-study/06-01-module-study/index2.py
@@ -0,0 +1,5 @@
+import module2
+print(module2.PI)
+
+a = module2.Math()
+print(a.solv(2))
